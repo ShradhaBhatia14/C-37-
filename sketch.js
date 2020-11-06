b7a511c4d86bf9f163950c622e15ae3951d4f5b9
Shradha <rajan.bhatia@hotmail.com>
2020-11-06 16:02:53 +0530

    moving camera and sprites

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var canvas, backgroundImage;
-
+var car1,car2,car3,car4;
+var cars;
 var gameState = 0;
 var playerCount;
 var allPlayers;
@@ -10,7 +11,7 @@ var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-50,displayHeight);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -25,5 +26,7 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+    drawSprites();
   }
+  
 }
